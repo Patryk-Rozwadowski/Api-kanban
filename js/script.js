3845c24c03804025af558d548999613c51dd93db
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-04-18 17:24:39 +0200

    15.5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function Phone(brand, price, color, date) {
+/*function Phone(brand, price, color, date) {
   this.brand = brand;
   this.price = price;
   this.color = color;
@@ -11,3 +11,34 @@ Phone.prototype.printInfo = function() {
 
 const iPhone6S = new Phone("Apple", 2250, "silver", 2015);
 iPhone6S.printInfo();
+*/
+
+/////////// SELF /////////
+
+function Button(text) {
+  this.text = text || 'Hello';
+}
+
+Button.prototype = {
+
+  create: function() {
+    const self = this;
+
+    this.element = document. createElement('button');
+    this.element.innerText = this.text;
+    this.element.addEventListener('click', function() {
+      alert(self.text);
+    });
+    document.body.appendChild(this.element);
+  }
+}
+
+const btn1 = new Button('Hello');
+const btn2 = new Button('Hello1');
+const btn3 = new Button('Hello2');
+const btn4 = new Button('Random text');
+
+btn1.create();
+btn2.create();
+btn3.create();
+btn4.create();
